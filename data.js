b6a76b020588cfa14cb65d9f16932122dd2c7125
Jumzer <bluevictor@hotmail.fr>
2023-03-22 17:20:22 +0100

    start linking data.js & display on html

--- data.js
+++ data.js
@@ -1,1 +1,50 @@
-const restaurantsList = [{}];
+const restaurantList = [
+  {
+    name: "restaurant1",
+    description: "this is a nice restaurant",
+    imgSrc: "/assets/fastfood.jpg",
+    imgAlt: "fastfood",
+    starRating: ".125",
+    foodPrice: "1",
+  },
+  {
+    name: "restaurant2",
+    description: "this is a nice restaurant",
+    imgSrc: "/assets/italien.jpg",
+    imgAlt: "italianfood",
+    starRating: ".125",
+    foodPrice: "2",
+  },
+  {
+    name: "restaurant3",
+    description: "this is a nice restaurant",
+    imgSrc: "assets/steakhouse.jpg",
+    imgAlt: "meatfood",
+    starRating: ".850",
+    foodPrice: "3",
+  },
+  {
+    name: "restaurant4",
+    description: "this is a nice restaurant",
+    imgSrc: "assets/veggie.jpg",
+    imgAlt: "veggie",
+    starRating: ".500",
+    foodPrice: "3",
+  },
+  {
+    name: "restaurant5",
+    description: "this is a nice restaurant",
+    imgSrc: "assets/powke.jpg",
+    imgAlt: "poke",
+    starRating: ".620",
+    foodPrice: "3",
+  },
+  {
+    name: "restaurant6",
+    description: "this is a nice restaurant",
+    imgSrc: "assets/spaghetti.jpg",
+    imgAlt: "spaghetti",
+    starRating: ".375",
+    foodPrice: "3",
+  },
+];
--- script.js
+++ script.js
@@ -1,28 +1,64 @@
 //------------Scrolling-Menu--------------------
 
-const scrollingMenu = document.querySelectorAll('.scrolling_menu');
-const scrollingMenuTitle = document.querySelectorAll('.scrolling_menu_title');
+const scrollingMenu = document.querySelectorAll(".scrolling_menu");
+const scrollingMenuTitle = document.querySelectorAll(".scrolling_menu_title");
 
 // récuperer les éléments des sous-menu (getElement )
-const sousMenu = document.querySelectorAll('.sous-menu');
+const sousMenu = document.querySelectorAll(".sous-menu");
 
 for (let i = 0; i < scrollingMenu.length; i++) {
-    scrollingMenu[i].addEventListener('click', function () {
-        if (sousMenu[i].style.display === 'inline') {
-            sousMenu[i].style.display = 'none'
-        } else {
-            sousMenu[i].style.display = 'inline'
-        }
-    })
+  scrollingMenu[i].addEventListener("click", function () {
+    if (sousMenu[i].style.display === "inline") {
+      sousMenu[i].style.display = "none";
+    } else {
+      sousMenu[i].style.display = "inline";
+    }
+  });
 }
 
 function updateTitle(balise, texte) {
-    // console.log(balise);
-    const target = document.getElementById(balise);
-    // console.log(target);
-    target.innerText = texte
+  // console.log(balise);
+  const target = document.getElementById(balise);
+  // console.log(target);
+  target.innerText = texte;
 }
 
+//------------Cards--------------------
 
-// BUDGET = SCROLLING_MENU a
-// VEGGIE = SOUS-MENU a
+let selectValue = "1";
+
+const restaurant = restaurantList[0];
+const restaurants = restaurantList;
+
+const section = document.getElementById("cardSection");
+
+const createCard = (restaurant) => {
+  const card = ` <div class="card">
+  <img src="${restaurant.imgSrc}" alt="${restaurant.imgAlt}" style="width: 100%" />
+  <div class="container">
+    <h4><b>${restaurant.name}</b></h4>
+    <p>${restaurant.description}</p>
+    <p> ${restaurant.foodPrice}</p>
+    <div class="star-ratings-css" title="${restaurant.starRating}"></div>
+  </div>
+</div>`;
+  const div = document.createElement("div");
+  div.innerHTML = card;
+  section.appendChild(div);
+};
+
+const render = () => {
+  section.innerHTML = "";
+  restaurants.forEach((restaurant) => {
+    if (selectValue === restaurant.foodPrice || selectValue === "1") {
+      createCard(restaurant);
+    }
+  });
+};
+
+render();
+
+function filter() {
+  selectValue = document.getElementsByClassName("food-price").value;
+  render();
+}
